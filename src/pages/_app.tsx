8aac26f09fc57722fb34af9e80a81727a11299dd
Muhammed Ashik <ashikpnc@gmail.com>
2023-07-17 19:08:31 +0530

    navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
+import NavBar from '@/components/NavBar';
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='app-container'>
+      <NavBar/>
+      <Component {...pageProps} />
+    </div>
+  ); 
 }
